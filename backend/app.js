3cd549e781df80f2958f0b7047fde85460e6ef9c
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-07-04 12:29:49 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,5 +25,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 });
 
+// hello
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
