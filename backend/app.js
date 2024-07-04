8be42b6130576f29de99e3b75ce22a7d6d221138
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-07-04 12:50:00 +0530

    I just Update New Changes
    I just Update New Changes

--- backend/app.js
+++ backend/app.js
@@ -19,15 +19,8 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
-});
-
 // hello
-app.get('/', (req, res) => res.send('Hello World!'));
-
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
